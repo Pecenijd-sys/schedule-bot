d319499a809d10621709264c499d0939af6d2f58
Pecenijd-sys <Pecenijd@gmail.com>
2026-09-07 22:36:40 +0300

    Update config.py

--- config.py
+++ config.py
@@ -9,12 +9,19 @@ if not BOT_TOKEN:
 
 ADMIN_ID = int(os.getenv("ADMIN_ID", "0"))
 
+# Оригинальная таблица университета (для XLSX-парсера)
 DEFAULT_SCHEDULE_URL = os.getenv(
     "SCHEDULE_URL",
-    "https://docs.google.com/spreadsheets/d/13rZhY4OYmWwwGfWaOJg4Maw7erKvnihlO79yJ1ZBkvg/export?format=csv&gid=952362142"
+    "https://docs.google.com/spreadsheets/d/13rZhY4OYmWwwGfWaOJg4Maw7erKvnihlO79yJ1ZBkvg/edit"
 )
 
-GROUPS = ["ІПЗ-11", "ІПЗ-12", "ІПЗ-13", "ІПЗ-14"]
+# Группы в том виде, как их извлекает парсер
+GROUPS = [
+    "група ІПЗ-11",
+    "група ІПЗ-12",
+    "група ІПЗ-13",
+    "група ІПЗ-14",
+]
 
 DAYS = ["Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця"]
 DAY_MAP = {
